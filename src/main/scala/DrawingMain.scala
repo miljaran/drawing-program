852fa775e4c1e13cd075c82497fe4c4a23a2b709
Milja Rantanen <milja.rantanen@gmail.com>
2022-03-14 01:11:24 +0200

    add scalafx dependency

--- src/main/scala/DrawingMain.scala
+++ src/main/scala/DrawingMain.scala
@@ -1,3 +1,13 @@
-object DrawingMain extends App {
-  println("Hello world")
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.scene.layout.Pane
+
+object DrawingMain extends JFXApp {
+  stage = new PrimaryStage {
+    title = "Hello world"
+    scene = new Scene {
+      root = new Pane
+    }
+  }
 }
