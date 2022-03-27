215a1c4d24156eb6ae2b84218960b2a7a48432e8
Milja Rantanen <milja.rantanen@gmail.com>
2022-03-28 02:15:24 +0300

    add color buttons

--- src/main/scala/DrawingMain.scala
+++ src/main/scala/DrawingMain.scala
@@ -3,10 +3,12 @@ import scalafx.application.JFXApp.PrimaryStage
 import scalafx.event.ActionEvent
 import scalafx.scene.Scene
 import scalafx.scene.canvas.Canvas
-import scalafx.scene.control.{Menu, MenuBar, MenuItem, Tab, TabPane}
-import scalafx.scene.input.MouseEvent
-import scalafx.scene.layout.{BorderPane, Pane}
+import scalafx.scene.control.{Menu, MenuBar, MenuItem, Tab, TabPane, ToggleButton, ToggleGroup}
+import scalafx.scene.layout.{BorderPane, ColumnConstraints, GridPane, RowConstraints}
 import scalafx.Includes._
+import scalafx.geometry.Insets
+import scalafx.scene.paint.Color
+import scalafx.scene.paint.Color.{Black, Blue, Cyan, DarkBlue, DarkViolet, FireBrick, Green, GreenYellow, Grey, Lime, Orange, Pink, Red, SaddleBrown, White, Yellow}
 
 object DrawingMain extends JFXApp {
 
@@ -25,6 +27,16 @@ object DrawingMain extends JFXApp {
     (drawing, tab, canvas)
   }
 
+  // Function to convert color rgb strings to hex strings
+  private def toHexString(c: Color) = {
+    val r = Math.round(c.getRed * 255).asInstanceOf[Int] << 24
+    val g = Math.round(c.getGreen * 255).asInstanceOf[Int] << 16
+    val b = Math.round(c.getBlue * 255).asInstanceOf[Int] << 8
+    val a = Math.round(c.getOpacity * 255).asInstanceOf[Int]
+    String.format("#%08X", r + g + b + a)
+  }
+
+
   // Create file menu
   val newItem = new MenuItem("New")
   val openItem = new MenuItem("Open")
@@ -42,6 +54,49 @@ object DrawingMain extends JFXApp {
   val menuBar = new MenuBar
   menuBar.menus = List(fileMenu, editMenu)
 
+  // Create toolbox and divide it into columns and rows
+  val toolbox = new GridPane
+  toolbox.setVgap(5)
+  toolbox.setHgap(5)
+  toolbox.setPadding(Insets(10, 10, 10, 10))
+  toolbox.setStyle("-fx-background-color: #cecace; -fx-grid-lines-visible: false")
+
+  val columnConstraints = Array.ofDim[ColumnConstraints](4)
+  for (i <- 0 to 3) {
+    columnConstraints(i) = new ColumnConstraints(50)
+  }
+  toolbox.columnConstraints = columnConstraints
+
+  val rowConstraints = Array.ofDim[RowConstraints](6)
+  for (i <- 0 to 5) {
+    rowConstraints(i) = new RowConstraints(50)
+  }
+  toolbox.rowConstraints = rowConstraints
+
+  // Create buttons for colors
+  var colors = new ToggleGroup
+  val colorButtons = Array.ofDim[ToggleButton](16)
+  for (i <- 0 to 15) {
+    colorButtons(i) = new ToggleButton
+  }
+  val colorArr = Array[Color](Red, Blue, Yellow, Orange, Lime, DarkViolet, White, Black, Grey, SaddleBrown, FireBrick, Pink, DarkBlue, Cyan, Green, GreenYellow)
+
+  // Set color button colors, sizes and toggle group
+  for (i <- 0 to 15) {
+    val button = colorButtons(i)
+    val hex = toHexString(colorArr(i))
+    colorButtons(i).setStyle(s"-fx-background-color: $hex")
+    colorButtons(i).setPrefSize(40, 40)
+    colorButtons(i).setToggleGroup(colors)
+  }
+
+  // Add color buttons to the toolbox
+  var m = 0
+  for (i <- 0 to 3; j <- 0 to 3) {
+    toolbox.add(colorButtons(m), i, j)
+    m += 1
+  }
+
   // Create first canvas
   val tabPane = new TabPane
   val (firstDrawing, firstTab, firstCanvas) = makeDrawingTab()
@@ -57,7 +112,7 @@ object DrawingMain extends JFXApp {
     scene = new Scene(800, 600) {
       root = new BorderPane {
         top = menuBar
-        // center = toolbox
+        center = toolbox
         right = tabPane
       }
     }
