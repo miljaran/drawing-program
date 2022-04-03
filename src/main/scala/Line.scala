7d0869bb937ab462c2e1917dfcf64b2bbb52bf1d
Milja Rantanen <milja.rantanen@gmail.com>
2022-04-03 23:30:22 +0300

    proper toString method for lines

--- src/main/scala/Line.scala
+++ src/main/scala/Line.scala
@@ -5,16 +5,20 @@ import scala.math._
 
 class Line(x_s: Double, y_s: Double, x_e: Double, y_e: Double, w: Double, h: Double, color: Color) extends Shape {
 
-  // Make sure no points are outside of the canvas
-  val x_start = max(0, x_s)
-  val y_start = max(0, y_s)
-  val x_end = min(w, x_e)
-  val y_end = min(h, y_e)
-
   // Draw line
   def draw(gc: GraphicsContext) = {
     gc.setStroke(color)
-    gc.strokeLine(x_start, y_start, x_end, y_end)
+    gc.strokeLine(x_s, y_s, x_e, y_e)
+  }
+
+  override def toString = {
+    // Make sure no points are outside of the canvas
+    val x_start = max(min(x_s, x_e), 0)
+    val y_start = max(min(y_s, y_e), 0)
+    val x_end = min(max(x_s, x_e), w)
+    val y_end = min(max(y_s, y_e), h)
+    val colorStr = colorToString(color)
+    s"L, $colorStr, $x_start, $y_start, $x_end, $y_end\n"
   }
 
 }
--- src/main/scala/Shape.scala
+++ src/main/scala/Shape.scala
@@ -1,7 +1,29 @@
 import scalafx.scene.canvas.GraphicsContext
+import scalafx.scene.paint.Color
+import scalafx.scene.paint.Color.{Black, Blue, Cyan, DarkBlue, DarkViolet, FireBrick, Green, GreenYellow, Grey, Lime, Orange, Pink, Red, SaddleBrown, White, Yellow}
 
 trait Shape {
 
   def draw(gc: GraphicsContext): Unit
 
+  def colorToString(c: Color) = {
+    c match {
+      case Red => "red"
+      case Blue => "blu"
+      case Yellow => "yel"
+      case Orange => "ora"
+      case Lime => "grn"
+      case DarkViolet => "pur"
+      case White => "whi"
+      case Black => "bla"
+      case Grey => "gre"
+      case SaddleBrown => "bro"
+      case FireBrick => "dre"
+      case Pink => "lre"
+      case DarkBlue => "dbl"
+      case Cyan => "lbl"
+      case Green => "dgr"
+      case GreenYellow => "lgr"
+    }
+  }
 }
